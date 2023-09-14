ce4ff677a59b764ff4a4f623b4a92f3c4125853c
0027026840 <hu.yiqiu2@iwhalecloud.com>
2023-09-14 14:51:12 +0800

    # test

--- main/main.go
+++ main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"geerpc"
+	"geerpc/xclient"
 	"log"
 	"net"
 	"sync"
@@ -18,41 +19,81 @@ func (foo Foo) Sum(args Args, reply *int) error {
 	return nil
 }
 
-func startServer(addr chan string) {
+func (foo Foo) Sleep(args Args, reply *int) error {
+	time.Sleep(time.Second * time.Duration(args.Num1))
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
+func startServer(addrCh chan string) {
 	var foo Foo
-	if err := geerpc.Register(&foo); err != nil {
-		log.Fatal("register error:", err)
+	l, _ := net.Listen("tcp", ":9091")
+	server := geerpc.NewServer()
+	_ = server.Register(&foo)
+	addrCh <- l.Addr().String()
+	server.Accept(l)
+}
+
+func foo(xc *xclient.XClient, ctx context.Context, typ, serviceMethod string, args *Args) {
+	var reply int
+	var err error
+	switch typ {
+	case "call":
+		err = xc.Call(ctx, serviceMethod, args, &reply)
+	case "broadcast":
+		err = xc.Broadcast(ctx, serviceMethod, args, &reply)
 	}
-	l, err := net.Listen("tcp", ":8090")
 	if err != nil {
-		log.Fatal("network error:", err)
+		log.Printf("%s %s error: %v", typ, serviceMethod, err)
+	} else {
+		log.Printf("%s %s success: %d + %d = %d", typ, serviceMethod, args.Num1, args.Num2, reply)
 	}
-	log.Println("start rpc server on", l.Addr())
-	addr <- l.Addr().String()
-	geerpc.Accept(l)
+}
+
+func call(addr1, addr2 string) {
+	d := xclient.NewMultiServerDiscovery([]string{"tcp@" + addr1, "tcp@" + addr2})
+	xc := xclient.NewXClient(d, xclient.RandomSelect, nil)
+	defer func() {_ = xc.Close()}()
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			foo(xc, context.Background(), "call", "Foo.Sum", &Args{Num1: i, Num2: i*i})
+		}(i)
+	}
+	wg.Wait()
+}
+
+func broadcast(addr1, addr2 string) {
+	d := xclient.NewMultiServerDiscovery([]string{"tcp@" + addr1, "tcp@" + addr2})
+	xc := xclient.NewXClient(d, xclient.RandomSelect, nil)
+	defer func() {_ = xc.Close()}()
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			foo(xc, context.Background(), "broadcast", "Foo.Sum", &Args{Num1: i, Num2: i*i})
+			ctx, _ := context.WithTimeout(context.Background(), time.Second*2)
+			foo(xc, ctx, "broadcast", "Foo.Sleep", &Args{Num1: i, Num2: i*i})
+		}(i)
+	}
+	wg.Wait()
 }
 
 func main() {
 	log.SetFlags(0)
-	addr := make(chan string)
-	go startServer(addr)
-	client, _ := geerpc.Dial("tcp", <-addr)
-	defer func(){ _ = client.Close() }()
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	// start two servers
+	go startServer(ch1)
+	go startServer(ch2)
+
+	addr1 := <-ch1
+	addr2 := <-ch2
 
 	time.Sleep(time.Second)
-	var wg sync.WaitGroup
-	for i := 0; i < 5; i ++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			args := &Args{Num1: i, Num2: i * 2}
-			ctx, _ := context.WithTimeout(context.Background(), time.Second)
-			var reply int
-			if err := client.Call(ctx, "Foo.Sum", args, &reply); err != nil {
-				log.Fatal("call Foo.Sum error:", err)
-			}
-			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
-		}(i)
-	}
-	wg.Wait()
+	call(addr1, addr2)
+	broadcast(addr1, addr2)
 }
