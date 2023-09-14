cdb9610b651302af460b657123b1e60dcd8ff6ab
0027026840 <hu.yiqiu2@iwhalecloud.com>
2023-09-14 17:56:31 +0800

    day7

--- xclient/discovery.go
+++ xclient/discovery.go
@@ -11,37 +11,42 @@ import (
 type SelectMode int
 
 const (
-	RandomSelect SelectMode = iota
-	RoundRobinSelect
+	RandomSelect     SelectMode = iota // select randomly
+	RoundRobinSelect                   // select using Robbin algorithm
 )
 
 type Discovery interface {
-	Refresh() error
-	Update(server []string) error
+	Refresh() error // refresh from remote registry
+	Update(servers []string) error
 	Get(mode SelectMode) (string, error)
 	GetAll() ([]string, error)
 }
 
 var _ Discovery = (*MultiServersDiscovery)(nil)
 
+// MultiServersDiscovery is a discovery for multi servers without a registry center
+// user provides the server addresses explicitly instead
 type MultiServersDiscovery struct {
-	r       *rand.Rand
-	mu      sync.RWMutex
+	r       *rand.Rand   // generate random number
+	mu      sync.RWMutex // protect following
 	servers []string
-	index   int
+	index   int // record the selected position for robin algorithm
 }
 
+// Refresh doesn't make sense for MultiServersDiscovery, so ignore it
 func (d *MultiServersDiscovery) Refresh() error {
 	return nil
 }
 
-func (d *MultiServersDiscovery) Update(server []string) error {
+// Update the servers of discovery dynamically if needed
+func (d *MultiServersDiscovery) Update(servers []string) error {
 	d.mu.Lock()
 	defer d.mu.Unlock()
-	d.servers = server
+	d.servers = servers
 	return nil
 }
 
+// Get a server according to mode
 func (d *MultiServersDiscovery) Get(mode SelectMode) (string, error) {
 	d.mu.Lock()
 	defer d.mu.Unlock()
@@ -53,7 +58,7 @@ func (d *MultiServersDiscovery) Get(mode SelectMode) (string, error) {
 	case RandomSelect:
 		return d.servers[d.r.Intn(n)], nil
 	case RoundRobinSelect:
-		s := d.servers[(d.index)%n]
+		s := d.servers[d.index%n] // servers could be updated, so mode n to ensure safety
 		d.index = (d.index + 1) % n
 		return s, nil
 	default:
@@ -61,19 +66,21 @@ func (d *MultiServersDiscovery) Get(mode SelectMode) (string, error) {
 	}
 }
 
+// returns all servers in discovery
 func (d *MultiServersDiscovery) GetAll() ([]string, error) {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-
-	servers := make([]string, len(d.servers))
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+	// return a copy of d.servers
+	servers := make([]string, len(d.servers), len(d.servers))
 	copy(servers, d.servers)
-	return d.servers, nil
+	return servers, nil
 }
 
-func NewMultiServersDiscovery(servers []string) *MultiServersDiscovery {
+// NewMultiServerDiscovery creates a MultiServersDiscovery instance
+func NewMultiServerDiscovery(servers []string) *MultiServersDiscovery {
 	d := &MultiServersDiscovery{
-		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
 		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
 	d.index = d.r.Intn(math.MaxInt32 - 1)
 	return d
--- xclient/xclient.go
+++ xclient/xclient.go
@@ -12,25 +12,21 @@ type XClient struct {
 	d       Discovery
 	mode    SelectMode
 	opt     *Option
-	mu      sync.Mutex
+	mu      sync.Mutex // protect following
 	clients map[string]*Client
 }
 
 var _ io.Closer = (*XClient)(nil)
 
 func NewXClient(d Discovery, mode SelectMode, opt *Option) *XClient {
-	return &XClient{
-		d: d,
-		mode: mode,
-		opt: opt,
-		clients: make(map[string]*Client),
-	}
+	return &XClient{d: d, mode: mode, opt: opt, clients: make(map[string]*Client)}
 }
 
 func (xc *XClient) Close() error {
 	xc.mu.Lock()
 	defer xc.mu.Unlock()
 	for key, client := range xc.clients {
+		// I have no idea how to deal with error, just ignore it.
 		_ = client.Close()
 		delete(xc.clients, key)
 	}
@@ -65,6 +61,9 @@ func (xc *XClient) call(rpcAddr string, ctx context.Context, serviceMethod strin
 	return client.Call(ctx, serviceMethod, args, reply)
 }
 
+// Call invokes the named function, waits for it to complete,
+// and returns its error status.
+// xc will choose a proper server.
 func (xc *XClient) Call(ctx context.Context, serviceMethod string, args, reply interface{}) error {
 	rpcAddr, err := xc.d.Get(xc.mode)
 	if err != nil {
@@ -73,15 +72,16 @@ func (xc *XClient) Call(ctx context.Context, serviceMethod string, args, reply i
 	return xc.call(rpcAddr, ctx, serviceMethod, args, reply)
 }
 
+// Broadcast invokes the named function for every server registered in discovery
 func (xc *XClient) Broadcast(ctx context.Context, serviceMethod string, args, reply interface{}) error {
 	servers, err := xc.d.GetAll()
 	if err != nil {
 		return err
 	}
 	var wg sync.WaitGroup
-	var mu sync.Mutex
+	var mu sync.Mutex // protect e and replyDone
 	var e error
-	replyDone := reply == nil
+	replyDone := reply == nil // if reply is nil, don't need to set value
 	ctx, cancel := context.WithCancel(ctx)
 	for _, rpcAddr := range servers {
 		wg.Add(1)
@@ -93,9 +93,9 @@ func (xc *XClient) Broadcast(ctx context.Context, serviceMethod string, args, re
 			}
 			err := xc.call(rpcAddr, ctx, serviceMethod, args, clonedReply)
 			mu.Lock()
-			if err != nil && e != nil {
+			if err != nil && e == nil {
 				e = err
-				cancel()
+				cancel() // if any call failed, cancel unfinished calls
 			}
 			if err == nil && !replyDone {
 				reflect.ValueOf(reply).Elem().Set(reflect.ValueOf(clonedReply).Elem())
